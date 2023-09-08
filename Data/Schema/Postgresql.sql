7b7e766e41942d5caac48ab57b1af33e14c46e0e
PaymonK <35069374+PaymonK@users.noreply.github.com>
2023-09-08 12:40:38 +0100

    Update Postgresql.sql

--- Data/Schema/Postgresql.sql
+++ Data/Schema/Postgresql.sql
@@ -1,1 +1,6 @@
 ...
+Download the create-db script and run it: https://github.com/sutton-institute/AutoRide/blob/main/Data/Schema/Postgresql.sql
+
+Then download the csv files and execute to add the records
+
+The end result is a Postgresql that is ready to use to see the application working with real-looking data.
