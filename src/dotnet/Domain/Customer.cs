81b7a66d65c48cfbc44dd1dd036c7196d8f6253a
PParhami <144446907+PParhami@users.noreply.github.com>
2023-09-15 10:59:45 +0100

    Update Customer.cs

--- src/dotnet/Domain/Customer.cs
+++ src/dotnet/Domain/Customer.cs
@@ -4,15 +4,15 @@ namespace AutoCar;
 
 public class Customer
 {
-    public Guid ID{get; set;}
-    public string Name{get; set;}
-    public string Family{get; set;}
-    public Genders Gender{get; set;}
-    public int Age{get; set;}
-    public string Email{get; set;}
-    public string Phone{get; set;}
-    public string Address{get; set;}
-    public string ZipCode{get; set;}
-    public DateTimeOffset JoinDate{get; set;} 
+    public Guid ID { get; set; }
+    public string Name { get; set; }
+    public string Family { get; set; }
+    public Genders Gender { get; set; }
+    public int? Age { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string? Address { get; set; }
+    public string ZipCode { get; set; }
+    public DateTimeOffset? JoinDate { get; set; }
 }
 
