6143ce7a4008834e9eceb50648b7dbe7de88b1c2
PParhami <144446907+PParhami@users.noreply.github.com>
2023-09-15 11:00:08 +0100

    Update Location.cs

--- src/dotnet/Domain/Location.cs
+++ src/dotnet/Domain/Location.cs
@@ -2,10 +2,11 @@ using System;
 
 namespace AutoCar;
 
-public class Location{
-    public Guid ID{get; set;}
-    public string Address{get; set;}
-    public double Latitude{get; set;}
-    public double Longitude{get; set;}
+public class Location
+{
+    public Guid ID { get; set; }
+    public string? Address { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
 }
 
