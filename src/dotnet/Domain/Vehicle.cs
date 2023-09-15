ddffe713a9b0086c7d271df0ac9af7010d3ba84f
PParhami <144446907+PParhami@users.noreply.github.com>
2023-09-15 11:05:57 +0100

    Update Vehicle.cs

--- src/dotnet/Domain/Vehicle.cs
+++ src/dotnet/Domain/Vehicle.cs
@@ -2,14 +2,15 @@ using System;
 
 namespace AutoCar;
 
-public class Vehicle{
-    public Guid ID{get; set;}
-    public Guid VehiclesID{get; set;}
-    public string Number{get; set;}
-    public VehicleStatus status{get; set;}
-    public DateTimeOffset LastService{get; set;} 
-    public Guid LastLocationID{get; set;}
-    public DateTimeOffset LastTime{get; set;}
-    
+public class Vehicle
+{
+    public Guid ID { get; set; }
+    public Guid VehicaleVasicInformationID { get; set; }
+    public string Number { get; set; }
+    public VehicleStatus status { get; set; }
+    public DateTimeOffset? LastService { get; set; }
+    public Guid? LastLocationID { get; set; }
+    public DateTimeOffset? LastTime { get; set; }
+
 }
 
