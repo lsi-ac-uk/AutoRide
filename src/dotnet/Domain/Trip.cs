f8b2fdc71f75a65b9dadcef8fa8a0a6cfae833ee
PParhami <144446907+PParhami@users.noreply.github.com>
2023-09-18 08:03:28 +0100

    Update Trip.cs
    Modify names

--- src/dotnet/Domain/Trip.cs
+++ src/dotnet/Domain/Trip.cs
@@ -7,13 +7,13 @@ public class Trip
     public Guid ID { get; set; }
     public Guid CustomerID { get; set; }
     public Guid VehicleID { get; set; }
+    public Guid? PaymentID { get; set; }
     public int StartLatitude { get; set; }
     public int StartLongitude { get; set; }
-    public int EndLatitude { get; set; }
-    public int EndLongitude { get; set; }
+    public int? EndLatitude { get; set; }
+    public int? EndLongitude { get; set; }
     public DateTimeOffset StartTripDate { get; set; }
     public DateTimeOffset? EndTripDate { get; set; }
-    public Guid? PaymentID { get; set; }
-    public TripStatus status { set; get; }
+    public TripStatus? status { set; get; }
 }
 
